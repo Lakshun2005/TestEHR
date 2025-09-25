104ed8979f5b3f7469cfba4b69662cb5547352d4
Lakshun2005 <lakshunbalaji@gmail.com>
2025-09-25 20:32:42 +0530

    Add files via upload

---
+++ app/analytics/page.tsx
@@ -0,0 +1,11 @@
+import { AnalyticsDashboard } from "@/components/analytics-dashboard"
+
+export default function AnalyticsPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <div className="container mx-auto p-8">
+        <AnalyticsDashboard />
+      </div>
+    </div>
+  )
+}
